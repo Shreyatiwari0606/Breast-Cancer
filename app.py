9f722fdd4ee0f4872392d9b4502e2216aea537af
Shreyatiwari0606 <tiwarishreya06062004@gmail.com>
2025-04-22 12:02:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -49,4 +50,5 @@ def predict():
         return render_template("index.html", prediction_text=f"Error: {e}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the port from the environment variable
+    app.run(host="0.0.0.0", port=port, debug=True)
